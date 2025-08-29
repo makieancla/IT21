0b5400b53c7681f8295ec50bac5569104adae1b1
makieancla <anclamcfrancis037@gmail.com>
2025-08-29 19:16:56 +0800

    added initial commit

--- hello.js
+++ hello.js
@@ -4,5 +4,11 @@ num1 =2;
 num2 =3;
 
 sum = num1 + num2;
+diff = num2 - num2;
+prod = num1 * num2;
+quo = num1 / num2;
 
 console.log("The sum of" + num1 +"and" + num2 +" is" + sum);
+console.log("The difference of" + num1 +"and" + num2 +" is" + diff);
+console.log("The product of" + num1 +"and" + num2 +" is" + prod);
+console.log("The qoutient of" + num1 +"and" + num2 +" is" + qou);
